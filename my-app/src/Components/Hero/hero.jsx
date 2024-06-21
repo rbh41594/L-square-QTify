2b77a464fb7629cce828ba984bc4293d26c819ed
rbh41594 <rbh415942gmail.com>
2024-06-21 22:13:02 +0530

    hero section basics added

---
+++ my-app/src/Components/Hero/hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./hero.css";
+import headphones from "../../Assets/headphones.svg";
+
+export function Hero() {
+  return (
+    <div className="hero">
+      <div>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over thousands podcast episodes</h1>
+      </div>
+      <div>
+        <img src={headphones} alt="headphones" width={122} />
+      </div>
+    </div>
+  );
+}
