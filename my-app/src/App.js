7f86ee7358e4ed23129abdfce59fbbe11367246a
rbh41594 <rbh415942gmail.com>
2024-06-21 22:15:00 +0530

    fixed mistake in app.js for hero section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Navbar from "./Components/Navbar/Navbar.jsx"
+import { Hero } from "./Components/Hero/hero.jsx";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
