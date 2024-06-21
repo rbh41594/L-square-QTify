cde50363656761e0d3b9e1c98905e9761fbabfc4
rbh41594 <rbh415942gmail.com>
2024-06-22 01:22:10 +0530

    card created

--- my-app/src/Components/Card/card.jsx
+++ my-app/src/Components/Card/card.jsx
@@ -1,27 +1,58 @@
-import React from 'react';
-import { Tooltip, Chip } from '@mui/material'
+import React from "react";
+import { Tooltip, Chip } from "@mui/material";
 
-export function Card ({data, type}) {
-    const { image, follows, title, slug, songs} = data;
+function Card({ data, type }) {
+  function getCard(type) {
+    switch (type) {
+      case "album": {
+        const { image, follows, title, slug, songs } = data;
+        return (
+          <Tooltip title={`${songs.length} songs`} placement="top" arrow>
+            <a href={`album/${slug}`}>
+              <div className="wrapper">
+                <div className="card">
+                  <img src={image} alt="album-image" loading="lazy" />
+                  <div className="banner">
+                    <Chip
+                      label={`${follows} Follows`}
+                      size="small"
+                      className="chip"
+                    />
+                  </div>
+                </div>
+                <div className="title">
+                  <p>{title}</p>
+                </div>
+              </div>
+            </a>
+          </Tooltip>
+        );
+      }
 
-    <Tooltip title={`${songs.length} songs`} placement='top' arrow>
-        <a href={`album/${slug}`}>
-            <div className='wrapper'>
-                <div className='card'>
-                    <img src={image} alt="album-image" loading='lazy' />
-                    <div className='banner'>
-                        <Chip 
-                        label={`${follows} Follows`}
-                        size='small'
-                        className='chip'
-                        />
-                    </div>
-                </div>
-                <div className='title'>
-                    <p>{title}</p>
-                </div>
+      case "song": {
+        const { image, likes, title } = data;
+        return (
+          <div className="wrapper">
+            <div className="card">
+              <img src={image} alt="song" loading="lazy" />
+              <div className="banner">
+                <Chip label={`${likes} likes`} size="small" className="chip" />
+              </div>
             </div>
-        </a>
-    </Tooltip>
+            <div className="title">
+              <p>{title}</p>
+            </div>
+          </div>
+        );
+      }
 
+      default: {
+        return <></>;
+      }
+    }
+  }
+
+  return <>{getCard(type)}</>;
 }
+
+export default Card;
